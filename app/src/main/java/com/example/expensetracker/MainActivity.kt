90e9cfa1a1a6a53417335f6301d1adfeb72dd627
Pratyush_Prasad <pratyushprasad80797@gmail.com>
2026-05-19 10:22:56 +0530

    Added expense input and list

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.expensetracker.ui.theme.ExpenseTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
             ExpenseTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +30,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Expense Tracker",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+
+    var amount by remember{
+        mutableStateOf("")
+    }
+    var expenses by remember{
+        mutableStateOf(listOf<String>())
+    }
+
+    Column(
+        modifier=modifier.fillMaxSize().padding(16.dp)
+    ){
+        Text(
+            text = "Expense Tracker",
+            fontSize=28.sp
+        )
+        Spacer(modifier=Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value=amount,
+            onValueChange = {
+                amount=it
+            },
+            label={
+                Text("Enter amount")
+            }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                if (amount.isNotEmpty()) {
+                    expenses = expenses + "₹$amount"
+                    amount = ""
+                }
+            }
+        ){
+            Text("Add expense")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        expenses.forEach {
+
+            Text(
+                text = it,
+                fontSize = 22.sp
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ExpenseTrackerTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
